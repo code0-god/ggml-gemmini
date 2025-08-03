6f4d550b4d5373209a4db4f6ac3d85966410ff34
zerogod <code0.sin@gmail.com>
2025-08-03 20:04:59 +0900

    fix: 차원 해석 수정

--- ggml-gemmini.cpp
+++ ggml-gemmini.cpp
@@ -15,14 +15,14 @@ static void ggml_backend_gemmini_mul_mat(
 
     // 0. 원본 FP32 입력 텐서
     const auto *src0 = dst->src[0];         // A:  I × K
-    const auto *src1 = dst->src[1];         // B:  J × K
+    const auto *src1 = dst->src[1];         // B:  K × J (But transpose되어 메모리상 J x K)
 
     DBG("dst shape:\n ne = [%llu, %llu, %llu, %llu]\n", dst->ne[0], dst->ne[1], dst->ne[2], dst->ne[3]);
     DBG("src0 shape:\n ne = [%llu, %llu, %llu, %llu]\n", src0->ne[0], src0->ne[1], src0->ne[2], src0->ne[3]);
     DBG("src1 shape:\n ne = [%llu, %llu, %llu, %llu]\n", src1->ne[0], src1->ne[1], src1->ne[2], src1->ne[3]);
 
-    const size_t I = src0->ne[1];   // A.ne[0] = K, A.ne[1] = I
-    const size_t J = src1->ne[0];   // B.ne[0] = J, B.ne[1] = K
+    const size_t I = src0->ne[1];   // I = A.ne[1], K = A.ne[0]
+    const size_t J = src1->ne[1];   // K = B.ne[0], J = B.ne[1] (transpose)
     const size_t K = src0->ne[0];
     DBG("I=%zu, J=%zu, K=%zu\n", I, J, K);
 
@@ -32,6 +32,8 @@ static void ggml_backend_gemmini_mul_mat(
     if (bias)
         ggml_gemmini_tensor<int32_t> tD(ctx->tmp_ctx, bias, ".i32");
 
+
+    /*
     // 3. 출력 버퍼 패딩
     const size_t row_pad_bytes = zerogod::align_up(J_pad * sizeof(int32_t), zerogod::GEMMINI_ROW_ALIGN);
     const size_t stride_e_C = row_pad_bytes / sizeof(int32_t);
@@ -86,6 +88,7 @@ static void ggml_backend_gemmini_mul_mat(
         memcpy(out_f + r * J,
                acc32 + r * row_stride,
                J * sizeof(float));
+    */
 }
 
 static void ggml_backend_gemmini_out_prod(ggml_backend_gemmini_context *ctx, struct ggml_tensor *dst)
@@ -110,39 +113,27 @@ static void ggml_backend_gemmini_free(ggml_backend_t backend)
     delete backend;
 }
 
-static enum ggml_status ggml_backend_gemmini_graph_compute(ggml_backend_t backend, struct ggml_cgraph *cgraph)
-{
-    ggml_backend_gemmini_context *ctx = (ggml_backend_gemmini_context *)backend->context;
+static enum ggml_status ggml_backend_gemmini_graph_compute(ggml_backend_t backend, struct ggml_cgraph * cgraph) {
+    ggml_backend_gemmini_context * ctx = (ggml_backend_gemmini_context *)backend->context;
 
-    // (1) bias_map 채우기
+    // (1) bias_map 갱신
+    ctx->bias_map.clear();
     for (int i = 0; i < cgraph->n_nodes; i++) {
         auto *node = cgraph->nodes[i];
-        if (node->op == GGML_OP_ADD && node->src[0]->op == GGML_OP_MUL_MAT) 
+        if (node->op == GGML_OP_ADD && node->src[0]->op == GGML_OP_MUL_MAT)
             ctx->bias_map[node->src[0]] = node->src[1];
     }
 
-    // (2) tmp_ctx 크기 계산
-    if (!ctx->tmp_ctx_initialized)
-    {
-        size_t total_bytes = 0, total_meta = 0;
-        std::set<ggml_tensor *> qset; // 텐서 중복 확인
-
-        // 메모리 계산
-        zerogod::ggml_calc_tmp_ctx_size(cgraph, ctx, sizeof(int8_t), qset, total_bytes, total_meta);
-
-        size_t ctx_bytes = total_bytes + total_meta + GGML_MEM_ALIGN;
-        posix_memalign(&ctx->arena, 16, ctx_bytes);
 
         struct ggml_init_params ip = {
-            /* .mem_size   = */ ctx_bytes,
-            /* .mem_buffer = */ ctx->arena,
-            /* .no_alloc   = */ true,
+            /* .mem_size   = */ 320ull * 1024 * 1024, // 320MiB
+            /* .mem_buffer = */ NULL,
+            /* .no_alloc   = */ false,
         };
 
         ctx->tmp_ctx = ggml_init(ip);
         GGML_ASSERT(ctx->tmp_ctx);
-        ctx->tmp_ctx_initialized = true;
-    }
+
 
     for (int i = 0; i < cgraph->n_nodes; i++)
     {
@@ -157,7 +148,7 @@ static enum ggml_status ggml_backend_gemmini_graph_compute(ggml_backend_t backen
                 bias = it->second;
 
             ggml_backend_gemmini_mul_mat(ctx, node, bias);
-            break;
+
         }
         case GGML_OP_OUT_PROD:
             // ggml_backend_gemmini_out_prod(ctx, node);
@@ -175,13 +166,6 @@ static enum ggml_status ggml_backend_gemmini_graph_compute(ggml_backend_t backen
         }
     }
     ctx->bias_map.clear();
-    ggml_free(ctx->tmp_ctx);
-    ctx->tmp_ctx = nullptr;
-
-    free(ctx->arena);
-    ctx->arena = nullptr;
-
-    ctx->tmp_ctx_initialized = false;
 
     return GGML_STATUS_SUCCESS;
 
